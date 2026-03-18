50cf24e51a6e94ef42613760be98d16b4f2c2068
BA10_95 <sooabadice@gmail.com>
2026-03-18 10:55:12 +0100

    Update DepotStock.java

--- src/main/java/DepotStock.java
+++ src/main/java/DepotStock.java
@@ -2,6 +2,3 @@ import java.time.LocalDateTime;
 public interface DepotStock {
     int getStock(String referenceArticle);
 }
-
-// Classe de commande créée par le service
-public record Commande(String identifiantClient, double total, LocalDateTime dateCreation) {}
